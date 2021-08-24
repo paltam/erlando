caef4dce03de506bbd58f52c189bf165bdd71078
Pallagi Tamás <Tamas.Pallagi@otpbank.hu>
2021-08-24 16:41:12 +0200

    Fixed deprecated parameterized modules by adding a dependency, fixed deprecated erlang:get_stacktrace/0 by using new try/catch syntax.

--- src/test.erl
+++ src/test.erl
@@ -76,6 +76,6 @@ hoist(ErrorT, Label, PlainFun) ->
                PlainFun(),
                return(passed)
            catch
-               Class:Reason ->
-                   fail({Label, Class, Reason, erlang:get_stacktrace()})
+               Class:Reason:Stack ->
+                   fail({Label, Class, Reason, Stack})
            end]).
